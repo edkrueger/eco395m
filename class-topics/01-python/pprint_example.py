d1db43a112e9fc654104393a4e97c92fff3474bf
edkrueger <edkrueger@gmail.com>
2023-09-19 17:08:01 -0500

    add in class activities.

---
+++ class-topics/01-python/pprint_example.py
@@ -0,0 +1,12 @@
+# import pprint
+from pprint import pprint
+
+
+realistic_dict = {
+    "hello": ["a greeting of welcome", "the first thing we say in a programming language"],
+    "world": ["a planet"],
+    "python": ["a snake"]
+}
+
+# pprint.pprint(realistic_dict)
+pprint(realistic_dict, indent=7)
