457ebf5e38756f9c0d2685cedfd04cc1ebf39847
edkrueger <22670796+edkrueger@users.noreply.github.com>
2025-09-04 17:00:46 -0500

    Add class exercises.

---
+++ class-topics/01-python/strings.py
@@ -0,0 +1,27 @@
+# print("Hello World")
+# print(str(1 + 2 + 3))
+# print("Hello" + "World")
+# print("Hello" * 3)
+# print("=" * 144)
+
+# print("a" < "b")
+
+# print(ord("a"))
+# print(ord("b"))
+
+# print("a" < "A")
+# print(ord("a"))
+# print(ord("A"))
+
+a = "Hello"
+print(a == "Hello")
+b = "World"
+
+print(a + " " + b)
+print(f"{a} {b}")
+
+print("Hello" + str(5))
+print("5" * 5)
+print(5 * 5)
+
+
