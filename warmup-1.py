3bee898e4fa3dc5758d39840b0424930c6bb605f
Blayne Burke <blayneburke@Blaynes-Air.lan>
2025-09-16 18:50:00 -0500

    Solve warmup-1 excercise 1.

--- warmup-1.py
+++ warmup-1.py
@@ -8,7 +8,8 @@ def sleep_in(weekday, vacation):
     sleep_in(True, False) → False
     sleep_in(False, True) → True
     """
-    return
+   
+    return not weekday or vacation
 
 
 def monkey_trouble(a_smile, b_smile):
